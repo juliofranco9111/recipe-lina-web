ce10706b1dcf25bb806aa3fc5e455cd9d3478ce2
Julio Franco <julio.franco9110@gmail.com>
2020-07-23 14:31:20 -0300

    primera subida

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(".my-rating").starRating({
+    starSize: 25,
+    useFullStars: true,
+    hoverColor: '#555',
+    ratedColor: '#f7931e',
+});
+
+$(".my-rating-grid").starRating({
+    starSize: 12,
+    useFullStars: true,
+    hoverColor: '#555',
+    ratedColor: '#f7931e',
+});
