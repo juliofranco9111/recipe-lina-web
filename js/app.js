a8c1e7f6954a1283dc50381d69bb41fb096fe8e7
Julio Franco <julio.franco9110@gmail.com>
2020-07-24 02:58:49 -0300

    second commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+$(".my-rating-large").starRating({
+    starSize: 65,
+    useFullStars: true,
+    hoverColor: '#555',
+    ratedColor: '#f7931e',
+});
 $(".my-rating").starRating({
     starSize: 25,
+    useFullStars: true,
+    hoverColor: '#555',
+    ratedColor: '#f7931e',
+});
+
+$(".my-rating-grid-large").starRating({
+    starSize: 33,
     useFullStars: true,
     hoverColor: '#555',
     ratedColor: '#f7931e',
